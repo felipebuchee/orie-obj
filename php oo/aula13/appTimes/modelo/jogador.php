89a554d123fdd0916f3049c175df18630ec401a7
Felipe Buche <felipe10buche@gmail.com>
2024-09-19 13:57:00 -0300

    .

---
+++ php oo/aula13/appTimes/modelo/jogador.php
@@ -0,0 +1,44 @@
+<?php 
+
+class Jogador{
+    private $nome;
+    private $numero;
+
+    
+
+    /**
+     * Get the value of nome
+     */
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    /**
+     * Set the value of nome
+     */
+    public function setNome($nome): self
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of numero
+     */
+    public function getNumero()
+    {
+        return $this->numero;
+    }
+
+    /**
+     * Set the value of numero
+     */
+    public function setNumero($numero): self
+    {
+        $this->numero = $numero;
+
+        return $this;
+    }
+}
---
+++ php oo/aula13/appTimes/modelo/time.php
@@ -0,0 +1,44 @@
+<?php
+
+class Time{
+    private $nome;
+    private $cidade;
+
+    
+
+    /**
+     * Get the value of nome
+     */
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    /**
+     * Set the value of nome
+     */
+    public function setNome($nome): self
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of cidade
+     */
+    public function getCidade()
+    {
+        return $this->cidade;
+    }
+
+    /**
+     * Set the value of cidade
+     */
+    public function setCidade($cidade): self
+    {
+        $this->cidade = $cidade;
+
+        return $this;
+    }
+}
